4c64db3affe00e4ab01523220c2bd3176d270620
david-carmigo <dlmarshall3@protonmail.com>
2023-04-22 00:09:49 -0500

    useContext...kind of?

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { createRoot } from "react-dom/client";
+import { useState } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import AdoptedPetContext from "./AdoptedPetContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,16 +16,19 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
+  const adoptedPet = useState(null);
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <header>
-          <Link to="/">Adopt Me(ow)!</Link>
-        </header>
-        <Routes>
-          <Route path="/details/:id" element={<Details />}></Route>
-          <Route path="/" element={<SearchParams />}></Route>
-        </Routes>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <header>
+            <Link to="/">Adopt Me(ow)!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />}></Route>
+            <Route path="/" element={<SearchParams />}></Route>
+          </Routes>
+        </AdoptedPetContext.Provider>
       </QueryClientProvider>
     </BrowserRouter>
   );
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,9 +1,13 @@
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import Carousel from "./Carousel";
 import fetchPet from "./fetchPet";
+import ErrorBoundary from "./ErrorBoundary";
+import Modal from "./Modal";
 
 const Details = () => {
+  const [showModal, setShowModal] = useState(false);
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
 
@@ -15,20 +19,41 @@ const Details = () => {
     );
   }
 
+  const handleAdoptButtonClick = () => {
+    setShowModal(!showModal);
+  };
+
   const pet = results.data.pets[0];
   return (
     <div className="details">
-      <Carousel images={pet.images}/>
+      <Carousel images={pet.images} />
       <div>
         <h1>{pet.name}</h1>
         <h2>
           {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
         </h2>
-        <button>Adopt {pet.name}</button>
+        <button onClick={handleAdoptButtonClick}>Adopt {pet.name}</button>
         <p>{pet.description}</p>
+        {showModal ? (
+          <Modal>
+            <div className="buttons">
+              <h1>Would you like to adopt {pet.name}?</h1>
+              <button onClick={handleAdoptButtonClick}>Yes</button>
+              <button onClick={handleAdoptButtonClick}>No</button>
+            </div>
+          </Modal>
+        ) : null}
       </div>
     </div>
-  )
+  );
 };
 
-export default Details;
+function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
+
+export default DetailsErrorBoundary;
